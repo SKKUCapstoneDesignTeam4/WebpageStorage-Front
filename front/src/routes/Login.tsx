abde0175e124488fd4e8d270d0d9914425db7067
beatheat <oh@beathe.at>
2022-10-30 15:23:05 +0900

    login system

--- front/src/routes/Login.tsx
+++ front/src/routes/Login.tsx
@@ -2,7 +2,16 @@ import * as React from 'react';
 import {Button, Input, Col, Row, Typography} from 'antd';
 import './Login.css';
 
+import axios from 'axios'
+import Password from 'antd/lib/input/Password';
+
+import Cookies from "universal-cookie";
+import {useNavigate } from "react-router-dom";
+
+const cookies = new Cookies();
+
 const {Title}=Typography;
+
 
 /**
  * Login area
@@ -11,6 +20,44 @@ const {Title}=Typography;
 
 
 export function Login(){
+    const [id, SetId] = React.useState("");
+    const [password, SetPassword] = React.useState("");
+    
+    const navigate = useNavigate();
+
+    const login = async () => {
+        
+        const response = await axios({
+            url: "http://localhost:4000/api/auth",
+            method: "post",
+            data: {
+                id: id, password: password
+            }
+        });
+        console.log(response.status);
+        if(response.status == 200){
+            const token = response.data.token;
+            cookies.set('access_token', token, {sameSite: 'strict'});
+            navigate('/Main');
+        }
+        //token있을시 token저장
+    }
+
+    const register = async () => {
+        const response = await axios({
+            url: "http://localhost:4000/api/register",
+            method: "post",
+            data: {
+                id: id, password: password
+            }
+        });
+        if(response.status == 200){
+            const token = response.data.token;
+            cookies.set('access_token', token, {sameSite: 'strict'});
+            navigate('/Main');
+        }
+    }
+    
     return(
         <Row>
             <Col flex={3}>
@@ -23,25 +70,32 @@ export function Login(){
                     <Row align='middle' justify='center'>
                         <Col>
                             <Input
-                                placeholder="ID" />
+                                id="id"
+                                placeholder="ID"
+                                value={id} 
+                                onChange={({ target: { value } }) => SetId(value)} 
+                            />
                         </Col>
                     </Row>
 
                     <Row align='middle' justify='center'>
                         <Col>
                             <Input.Password
+                                id="password"
                                 placeholder="Password"
+                                value={password} 
+                                onChange={({ target: { value } }) => SetPassword(value)} 
                             /> 
                         </Col>
                     </Row>
 
                     <Row>
                         <Col span={4}>
-                            <Button type='primary'>Login</Button>
+                            <Button type='primary' onClick={login}>Login</Button>
                         </Col> 
                     
                         <Col span={4} offset={8}>
-                            <Button type='primary'>Resister</Button>
+                            <Button type='primary' onClick={register}>Resister</Button>
                         </Col>
                     </Row>
                 </div>
