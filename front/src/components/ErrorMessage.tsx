7a3b5666dec30e00109230a8880f2325f36c6990
skfsjrnfl <81664471+skfsjrnfl@users.noreply.github.com>
2022-10-31 23:24:32 +0900

    make error message window

---
+++ front/src/components/ErrorMessage.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { Button, Col, Row, Typography } from 'antd';
+import Column from 'antd/lib/table/Column';
+
+import './design/ErrorMessage.css'
+const { Title, Text } = Typography;
+
+type MessageProps ={
+    title: string;
+    message: string;
+    func(): void;
+}
+
+export function ErrorMessage({title, message, func} : MessageProps) {
+  return (
+    <>
+      <Modal className='ErrorMessage' isOpen={true}>
+        
+            <Row>
+                <Col>
+                    <Title level={3}>{title}</Title>
+                </Col>
+            </Row>
+            <Row >
+                <Col>
+                    <Text strong>{message}</Text>
+                </Col>
+            </Row>
+            <Row >
+                <Col>
+                    <Button size="small" onClick={func}>Close</Button>
+                </Col>
+            </Row>
+      </Modal>
+    </>
+  )
+} 
+
+export default ErrorMessage;
